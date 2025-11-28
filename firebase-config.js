454e750be727bf67ea1e005f58fd4669285ed32f
Tushar <tushar@example.com>
2025-11-28 17:24:02 +0530

    Add Firebase credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,11 @@
 // Firebase Configuration
-// Get these values from your Firebase Console: https://console.firebase.google.com/
-
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA_2KfVUdtUh0D8iK7hx6KcN7107dJlwRE",
+  authDomain: "journal-4b15b.firebaseapp.com",
+  projectId: "journal-4b15b",
+  storageBucket: "journal-4b15b.firebasestorage.app",
+  messagingSenderId: "869823757728",
+  appId: "1:869823757728:web:66cb38fffcd2f58b5038c5"
 };
 
 // Initialize Firebase
